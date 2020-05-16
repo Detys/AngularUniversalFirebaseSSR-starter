b03a5fbb4744680add07baa12f4e3724ddb1ee81
Kristian Santrou <k.santrou@gmail.com>
2020-05-16 11:17:51 +0300

    Added about component and routing

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
